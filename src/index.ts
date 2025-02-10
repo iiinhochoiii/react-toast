ef19cd0727c204a5a4812a7aa5f957e676865005
iiinhochoiii <dlsgh120@gmail.com>
2025-02-10 15:50:40 +0900

    chore: Toast export name & version update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default } from "./components/Toast/ToastContainer";
+export { default as Toast } from "./components/Toast/ToastContainer";
 export { addToast as onToast } from "./core/store";
 export type { ToastType } from "./types/toast";
